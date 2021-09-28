698effaa215ffe9a90ea28ecaa4ba973b474d254
prav-tadikonda <prav@humminghomes.com>
2021-09-28 11:06:38 -0400

    add in homes display interview question

--- take-home/api-interaction/App.js
+++
@@ -1,1 +0,0 @@
-//List to API: https://api.jsonbin.io/b/5f1b0596c58dc34bf5d9e9a9
---
+++ take-home/homes-display/src/App.js
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
+
+let API = axios.create({
+  baseURL: "https://api.jsonbin.io/b/5f1b0596c58dc34bf5d9e9a9",
+  responseType: "json",
+});
+
+const App = () => {
+  const [properties, setProperties] = useState({});
+
+  useEffect(() => {
+    API.get("/").then((response) => {
+      setProperties(response.data.properties)
+
+      // The API response
+      console.log(response.data)
+    });
+  }, []);
+
+  console.log(properties)
+
+  return (
+    <div className="App">
+      <header className="App-header">Homes Display Task</header>
+      <div className="instructions">
+        <p>
+          <strong>Step 1:</strong> Display list of homes
+        </p>
+        <p>
+          <strong>Step 2:</strong> When the list of homes is displayed, be able
+          to click on each home and see specific information
+        </p>
+        <p className="tips">
+          *You may import any additional libraries (ant design, rebass, etc.)
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default App;
